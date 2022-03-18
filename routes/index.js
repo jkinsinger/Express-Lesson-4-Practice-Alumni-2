5f7ab2322dd32a2709e337bd8bf116f6aecb7131
jkinisnger <joy.kinsinger02@gmail.com>
2022-03-18 07:38:47 -0500

    running queries - running a get all actors query

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require ('mysql2');
+const mysql = require ('mysql2');
+const models = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/actors', function(req, res, next){
+  models.actor.findAll({}).then(actorsFound => {
+    res.render('actors', {
+      actors: actorsFound
+    });
+  });
+});
+
 
 module.exports = router;
